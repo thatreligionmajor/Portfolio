4f411dd52244fbabc4adb43c0759ac9f63bf3a83
Theresa Barkasy <112668448+thatreligionmajor@users.noreply.github.com>
2024-02-24 01:30:19 +0000

    did away with manual styling for the profile image and applied bootstrap styling

--- src/components/ProfileSection.jsx
+++ src/components/ProfileSection.jsx
@@ -1,17 +1,17 @@
 const PortfolioSection = () => {
     return (
     <div class="card mb-3 border-dark">
-        <div class="row g-0 fade-up">
+        <div class="row g-0 fade-up mx-5">
             <div class="col-md-4">
-            <img src="/src/assets/claymationprofile.jpeg" class="img-fluid rounded-start " alt="" />
+                <img src="/src/assets/claymationprofile.jpeg" class="w-75 img-fluid rounded-start" alt="" />
             </div>
             <div class="col-md-8">
                 <div class="card-body">
-                    <h2 class="card-title">Hi, I'm Theresa</h2>
-                    <h6>
+                    <h2 class="card-title mb-4">Hi, I'm Theresa</h2>
+                    <h3 class="mb-3">
                         Developer, Banjo Player, and D&D Nerd
-                    </h6>
-                    <p class="card-text text-start ps-3">
+                    </h3>
+                    <p class="card-text text-start ps-3 mx-5">
                     As a software engineer with a deep appreciation for the humanities and religious studies, I bring a unique perspective to the tech industry. My journey began with a fascination for understanding the intricate narratives of cultures and belief systems. Today, I use my technical skills to create solutions that make a positive impact on our world. 🌿
                     <br/>
                     Beyond coding, I'm passionate about the intersection of technology and social change.
