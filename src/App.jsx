3531578f873aa321c8f61db828f7f6992df03c94
Theresa Barkasy <112668448+thatreligionmajor@users.noreply.github.com>
2024-03-19 14:56:19 +0000

    features to <ul>; spacing of bio

--- src/App.jsx
+++ src/App.jsx
@@ -18,25 +18,18 @@ function App() {
             <br />
             <h6>Features:</h6>
             <br />
-            Interactive Menu: Explore a curated menu of molecular gastronomy dishes, each with detailed descriptions and images.
-            <br />
-            Order Placement: Easily navigate through the menu to select and place orders for your desired dishes.
-            <br />
-            Dynamic Navigation: Seamless navigation between different food items, categories, and sections of the website.
-            <br />
-            Admin Management: Add and delete admin users to manage website operations securely.
-            <br />
-            Order Management: View, manage, and track orders placed by customers in real time. Update order statuses to keep customers informed about their order progress.
-            <br />
-            Menu Management: Add, update, and delete menu items with ease, allowing for flexibility in showcasing culinary creations. Organize menu items into categories for better navigation and presentation.
-            <br />
-            Authentication: Secure login system for admin users to access backend functionalities and perform administrative tasks.
-            <br />
-            Data Persistence: Utilize databases to store and retrieve information about menu items, orders, and admin users for seamless data management.
-            <br />
-            User-Friendly Interface: Intuitive and responsive user interface design for both frontend and backend, ensuring a pleasant browsing and administrative experience.
-            <br />
-            Security Measures: Implement security measures to protect sensitive information and prevent unauthorized access to administrative features.
+            <ul>
+              <li>Interactive Menu: Explore a curated menu of molecular gastronomy dishes, each with detailed descriptions and images.</li>
+              <li>Order Placement: Easily navigate through the menu to select and place orders for your desired dishes.</li>
+              <li>Dynamic Navigation: Seamless navigation between different food items, categories, and sections of the website.</li>
+              <li>Admin Management: Add and delete admin users to manage website operations securely.</li>
+              <li>Order Management: View, manage, and track orders placed by customers in real time. Update order statuses to keep customers informed about their order progress.</li>
+              <li>Menu Management: Add, update, and delete menu items with ease, allowing for flexibility in showcasing culinary creations. Organize menu items into categories for better navigation and presentation.</li>
+              <li>Authentication: Secure login system for admin users to access backend functionalities and perform administrative tasks.</li>
+              <li>Data Persistence: Utilize databases to store and retrieve information about menu items, orders, and admin users for seamless data management.</li>
+              <li>User-Friendly Interface: Intuitive and responsive user interface design for both frontend and backend, ensuring a pleasant browsing and administrative experience.</li>
+              <li>Security Measures: Implement security measures to protect sensitive information and prevent unauthorized access to administrative features.</li>
+            </ul>
           </>
         ),
       image: "/assets/quantum-eats-desktop-homepage.png",
@@ -49,18 +42,16 @@ function App() {
       description: 
         (
           <>
-          An ironic and playful comic book convention app created from scratch, using back and front end technologies such as Python, Flask, SQL, JavaScript, React, and REST APIs.
-          <br/>
-          <br />
-          <h6>Features:</h6>
-          <br />
-          Event Information: Display detailed information about the event, including dates, location, and schedule.
-          <br />
-          Guests and Speakers: Showcase a list of special guests, speakers, and panelists attending the event. Preview their work on site.
-          <br />
-          Exhibitors and Vendors: Highlight exhibitors and vendors participating in the convention, along with their products and services.
-          <br />
-          Schedule and Agenda: Present a comprehensive schedule and agenda of events, including panels, workshops, screenings, and performances.
+            An ironic and playful comic book convention app created from scratch, using back and front end technologies such as Python, Flask, SQL, JavaScript, React, and REST APIs.
+            <br/>
+            <br />
+            <h6>Features:</h6>
+            <ul>
+              <li>Event Information: Display detailed information about the event, including dates, location, and schedule.</li>
+              <li>Guests and Speakers: Showcase a list of special guests, speakers, and panelists attending the event. Preview their work on site.</li>
+              <li>Exhibitors and Vendors: Highlight exhibitors and vendors participating in the convention, along with their products and services.</li>
+              <li>Schedule and Agenda: Present a comprehensive schedule and agenda of events, including panels, workshops, screenings, and performances.</li>
+            </ul>
           </>
         ),
       image: "/assets/worst-con-ever.png",
--- src/components/ProfileSection.jsx
+++ src/components/ProfileSection.jsx
@@ -8,14 +8,15 @@ const PortfolioSection = () => {
             <div class="col-md-8">
                 <div class="card-body">
                     <h2 class="card-title mb-4">Hi, I'm Theresa</h2>
-                    <h3 class="mb-3">
+                    <h3 class="mb-5">
                         Developer, Banjo Player, and D&D Nerd
                     </h3>
-                    <p class="card-text text-start ps-3 mx-5">
-                        As a software engineer with a deep appreciation for the humanities and religious studies, I bring a unique perspective to the tech industry. My journey began with a fascination for understanding the intricate narratives of cultures and belief systems. Today, I use my technical skills to create solutions that make a positive impact on our world. 🌿
-                        <br/>
+                    <h5 class="card-text text-start ps-3 mx-5 mb-5">
+                        As a software engineer with a deep appreciation for the humanities and religious studies, I bring a unique perspective to the tech industry. My journey began with a fascination for understanding the intricate narratives of cultures and belief systems. Today, I use my technical skills to create solutions that make a positive impact on our world. 🌿 
+                    </h5>
+                    <h5 class="card-text text-start ps-3 mx-5">
                         Beyond coding, I'm passionate about the intersection of technology and social change.
-                        </p>
+                    </h5>
                     <div class="d-flex flex-row gap-3 justify-content-start">
                         
                     </div>
